8b12ff5fd9125e4388895ba5d85ba8f824453924
Tara.Baker <tara.miranda@live.com>
2022-12-10 10:41:19 -0500

    Modifying some styles, adding new project files

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,11 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Tara.
           </h1>
+          <h2 className="text-white">
+          I'm a full stack software developer in Southwestern Ontario.
+          </h2>
           <p className="mb-8 leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
             laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
@@ -19,12 +20,12 @@ export default function About() {
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              Get in Touch
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+              What I've Been Working On
             </a>
           </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -1,54 +1,42 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Brantford Homes for Sale",
+    subtitle: "WordPress",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "A website for a local realtor featuring properties for sale, contact forms and the Realty Press plugin.",
+    image: "./brantfordHomes.gif",
+    link: "https://brantfordhomesforsale.ca",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Wayne Leeder Trucking",
+    subtitle: "WordPress",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "A simple portfolio website for a local trucking company, providing a description of their services and a contact form.",
+    image: "./wayneLeeder.gif",
+    link: "https://wayneleedertrucking.ca",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "A & C Landscaping",
+    subtitle: "WordPress",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "A simple portfolio website for a local landscaping company, providing a description of their services and a contact form",
+    image: "./aAndC.gif",
+    link: "https://aandclandscape.ca",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Nathan's Reno and Tile",
+    subtitle: "WordPress",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "A simple portfolio website for a local tiling and renovation company, providing a description of their services and a contact form",
+    image: "./nathan.gif",
+    link: "https://nathansrenoandtile.ca/",
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
+export const resume = {
+  title: "Resume",
+  file: "./taraBakerResume.pdf"
+};
 
 export const skills = [
   "JavaScript",
